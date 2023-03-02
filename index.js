13d1bc5f5f67779319173d234da68eac79eaa741
Jack Ngo <83608215+j-ngo828@users.noreply.github.com>
2023-03-01 22:32:38 -0500

    Finalize project

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function createGrid(width, height) {
+  const createdCanvas = document.createElement("div");
+  createdCanvas.classList.add("canvas");
+  document.querySelector("#app").appendChild(createdCanvas);
   const canvas = document.querySelector(".canvas");
   for (let row = 0; row < height; ++row) {
     const rowContainer = document.createElement("div");
@@ -12,16 +15,36 @@ function createGrid(width, height) {
   }
 }
 
+function clearGrid() {
+  const canvas = document.querySelector(".canvas");
+  if (!canvas) return;
+  document.querySelector("#app").removeChild(canvas);
+}
+
 function main() {
-  createGrid(16, 16);
-  const pixels = document.querySelectorAll(".pixel");
-  const changeColor = (event) => {
-    console.log("ENTERED");
-    event.target.classList.add("paint");
-  };
-  pixels.forEach((pixel) => {
-    pixel.addEventListener("mouseenter", changeColor);
-    // pixel.addEventListener("mouseleave", () => console.log("BYE!"));
+  const inputButton = document.querySelector(".grid-input-size");
+  inputButton.addEventListener("click", (event) => {
+    let size = prompt("Enter a size for the grid: ");
+    while (true) {
+      if (size === null) return;
+      size = parseInt(size);
+      if (isNaN(size) || size < 1 || size > 100) {
+        size = prompt("Please enter a number between 1 and 100 ");
+        continue;
+      }
+      break;
+    }
+    clearGrid();
+    createGrid(size, size);
+    const pixels = document.querySelectorAll(".pixel");
+    const changeColor = (event) => {
+      console.log("ENTERED");
+      event.target.classList.add("paint");
+    };
+    pixels.forEach((pixel) => {
+      pixel.addEventListener("mouseenter", changeColor);
+      // pixel.addEventListener("mouseleave", () => console.log("BYE!"));
+    });
   });
 }
 
